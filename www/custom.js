f45213a4ee1aa5fede523daebf9fc48fb8f15314
Zhivka Dimova <ji.gancheva@gmail.com>
2019-12-16 14:05:42 +0100

    Initialize sintodental repository

---
+++ www/custom.js
@@ -0,0 +1,22 @@
+
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#3c404d",
+        "text": "#d6d6d6"
+      },
+      "button": {
+        "background": "#67bc00",
+        "text": "#fff"
+      }
+    },
+    "position": "bottom-right",
+    "content": {
+      "message": "Ние използваме бисквитки, за да ви осигурим най-доброто използване на нашия уеб сайт. Използвайки сайта ни вие се съгласявате с нашата политика за използване на \"бисквитки\"",
+      "dismiss": "Разбрах",
+      "link": "Научи повече",
+      "href": "/cookie-policy.html"
+    }
+  })
+});
